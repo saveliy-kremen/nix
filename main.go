bd6a7c813c47d385d09cfc4eba2736671af5493d
Станіслав Д <saveliy4kremen@gmail.com>
2021-03-13 07:06:18 +0200

    Task 5 with struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,22 +9,29 @@ import (
 	"strconv"
 )
 
+type Post struct {
+	UserID int `json:"userId"`
+	Id     int
+	Title  string
+	Body   string
+}
+
 func main() {
 	directory := "./storage/posts/"
 	if _, err := os.Stat(directory); err != nil {
 		os.MkdirAll(directory, 0775)
 	}
 
-	ch := make(chan []byte)
+	ch := make(chan Post)
 	for i := 0; i < 100; i++ {
 		go MakeRequest("https://jsonplaceholder.typicode.com/posts/"+strconv.Itoa(i), ch)
 	}
 	for i := 0; i < 100; i++ {
-		ioutil.WriteFile("./storage/posts/"+strconv.Itoa(i+1)+".txt", <-ch, 0644)
+		ioutil.WriteFile("./storage/posts/"+strconv.Itoa(i+1)+".txt", []byte(fmt.Sprintf("%+v\n", <-ch)), 0644)
 	}
 }
 
-func MakeRequest(url string, ch chan []byte) {
+func MakeRequest(url string, ch chan Post) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
@@ -31,5 +39,11 @@ func MakeRequest(url string, ch chan []byte) {
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	ch <- body
+	post := Post{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		fmt.Println(err)
+		panic("json decode error")
+	}
+	ch <- post
 }
