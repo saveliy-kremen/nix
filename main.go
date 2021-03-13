8c67ceeb19a9002301cbb59aa112c05b798c9e29
Станіслав Д <saveliy4kremen@gmail.com>
2021-03-13 06:10:48 +0200

    Task 3

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-	fmt.Println("Hello, NIX Education")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts") 
+	if err != nil { 
+		fmt.Println(err) 
+		return 
+	} 
+	defer resp.Body.Close()
+	for true {
+		bs := make([]byte, 1024)
+		n, err := resp.Body.Read(bs)
+		fmt.Print(string(bs[:n]))
+		if n == 0 || err != nil{
+			break
+		}
+	}
 }
